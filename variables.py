667ae36ab20f857843f03e6e74373ccacd9511a3
Tom Jamieson <tom_jamieson@Outlook.com>
2024-11-10 10:58:26 +1100

    Added variables.py to define and print SpaceX data

---
+++ variables.py
@@ -0,0 +1,15 @@
+# Number of launches
+launch_count = 10
+
+# Name of the rocket
+rocket_name = "Falcon 9"
+
+# Launch success rate
+success_rate = 0.95
+
+# launch_count: Integer representing the number of rocket launches.
+print("Number of launches:", launch_count)
+# rocket_name: String containing the name of the rocket.
+print("Name of the rocket:", rocket_name)
+# success_rate: Float representing the success rate of the launches.
+print("Launch success rate:", success_rate)
